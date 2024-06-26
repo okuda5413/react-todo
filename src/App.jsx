1c85ec84b9eaf799a75c629d9581b358bbe592a3
Olga Kudakhtina <olgakudakhtina@MacBook-Pro-3.local>
2024-06-26 06:43:07 -0700

    Project set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import React from 'react';
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) => <li key={number.toString()}>{number}</li>);
+  return <ul>{listItems}</ul>;
 }
 
-export default App
+function App() {
+  const numbersArray = [1, 2, 3, 4, 5];
+  return (
+    <div>
+      <h1>Number List</h1>
+      <NumberList numbers={numbersArray} />
+    </div>
+  );
+}
+
+export default App;
